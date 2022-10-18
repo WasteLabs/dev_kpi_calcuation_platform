c88a06ea718812cffd2c4329bb07ad4ed928191c
Adil <32989359+AtmosOne@users.noreply.github.com>
2022-10-18 13:52:43 +0600

    fix: query preprocessing sequence

--- src/osrm/drivers/models.py
+++ src/osrm/drivers/models.py
@@ -5,9 +5,9 @@ from ..abstract import AbstractParameters
 
 
 class RouteParameters(AbstractParameters):
-    geometry: Literal["polyline", "polyline6", "geojson"] = Field(default='geojson')
     overview: Literal["full", "false", "simplified"] = Field(default='full')
     steps: Literal["true", "false"] = Field(default='true')
-    alternatives: Literal["true", "false"] = Field(default='true')
+    alternatives: Literal["true", "false"] = Field(default='false')
+    geometries: Literal["polyline", "polyline6", "geojson"] = Field(default='geojson')
     annotations: Literal["true", "false"] = Field(default='true')
     continue_straight: Literal["true", "false"] = Field(default='true')
