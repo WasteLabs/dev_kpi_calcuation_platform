765736abb2c1b964ad5527b75683adb0093c2102
Adil <32989359+AtmosOne@users.noreply.github.com>
2022-11-03 01:03:54 +0600

    feat: setup parser of route_sequence

--- src/osrm/abstract.py
+++ src/osrm/abstract.py
@@ -1,6 +1,7 @@
 from copy import deepcopy
 import logging
 from typing import Any
+from typing import Union
 
 from pydantic import BaseModel
 from pydantic import Field
@@ -28,8 +29,8 @@ class AbstractParameters(BaseModel):
 class AbstractParser(BaseModel):
     content: dict[str, Any] = Field(description="Response content")
 
-    def __generate_exc(self, key_sequence: list[str]):
-        msg = f"Incorrect key in key sequence: {key_sequence}"
+    def __generate_exc(self, keys: Union[list[str], str]):
+        msg = f"Incorrect key or key sequence is provided: {keys}"
         logging.error(msg)
         raise RuntimeError(msg)
 
@@ -44,8 +45,18 @@ class AbstractParser(BaseModel):
                 value = value[key]
             return value
         except KeyError:
-            self.__generate_exc(key_sequence=key_sequence)
+            self.__generate_exc(keys=key_sequence)
         except TypeError:
-            self.__generate_exc(key_sequence=key_sequence)
+            self.__generate_exc(keys=key_sequence)
         except IndexError:
-            self.__generate_exc(key_sequence=key_sequence)
+            self.__generate_exc(keys=key_sequence)
+
+    def _get_pandas_field(
+            self,
+            df: dict[str, Any],
+            key: str,
+    ) -> list[Any]:
+        try:
+            return df[key].to_list()
+        except KeyError:
+            self.__generate_exc(keys=key)
